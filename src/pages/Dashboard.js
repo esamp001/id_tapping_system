6c64a7d7965d7937ec2af934a03479c33450f6c9
ellejun sampaga <ellejunsampaga@gmail.com>
2025-10-28 10:00:00 +0800

    Auto commit: 10/28/2025, 10:00:00 AM

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Typography, Box, Paper } from '@mui/material'
 
 const Dashboard = () => {
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', p: 3 }}>
+            <Paper>
+                <Typography>Alice Johnson</Typography>
+                <Typography>ID: 001</Typography>
+                <Typography>time</Typography>
+                <Typography>Tapped in</Typography>
+            </Paper>
         </Box>
     )
 }
