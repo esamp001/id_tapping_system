1843342013bbfa2b791b692876afc77d0320874e
ellejun sampaga <ellejunsampaga@gmail.com>
2025-10-29 11:00:00 +0800

    Auto commit: 10/29/2025, 11:00:00 AM

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,5 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/test-db', async (req, res) => {
-    try {
-        const result = await db.raw('SELECT 1+1 AS result');
-        console.log(' Database connected successfully!');
-        res.json({ message: 'Database connected!', result: result.rows });
-    } catch (error) {
-        console.error(' Database connection failed:', error.message);
-        res.status(500).json({ message: 'Database connection failed', error: error.message });
-    }
-});
-
 // Run server
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
